1e25904c2186f92c1bf510d0d5f6d41ba43ae019
Hui <jeffrey.x.hui@gmail.com>
2019-11-10 07:17:15 -0800

    added routes inside jumbotron, wrapped nav with Links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import {Jumbotron} from 'reactstrap';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+  <>
+    <div>
+      <Navigation />
+    </div>
+    <Jumbotron>
+      <Route exact path='/' render={
+        () => {
+          return (<h1>Home</h1>);
+        }
+      }></Route>
+      <Route path='/about' component={About}></Route>
+      <Route path='/contact' component={Contact}></Route>
+    </Jumbotron>
+  </>
 );
 
 export default App;
